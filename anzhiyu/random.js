1eb928141277a626dab626a765b07c4e8b55f3f7
wilsonhall001789 <weiqin001789@hotmail.com>
2026-06-05 09:47:38 +0800

    Site updated: 2026-06-05 09:47:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/04/六月的风，吹过青春的分水岭：关于选择、成长与未来/"];function toRandomPost(){
+var posts=["2026/06/05/爆冷！法国队1-2遭科特迪瓦绝杀，15亿豪阵露软肋，2026世界杯变天了？/","2026/06/04/六月的风，吹过青春的分水岭：关于选择、成长与未来/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
